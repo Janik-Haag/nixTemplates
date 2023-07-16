e392746e7ef546a5cdefeffa1215514d8fb33194
Janik H <janik@aq0.de>
2023-07-16 06:08:02 +0200

    template/trivial: init

---
+++ trivial/flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [ cowsay ];
+        };
+      });
+}
